bcdd9d41685c72e2c2c67403fef45f564959f4f8
Tom Antony <tomantony1997@gmail.com>
2021-05-30 12:00:34 +0530

    new

---
+++ script.js
@@ -0,0 +1,9 @@
+var counter=3;
+function upvote() {
+    counter++;
+    document.getElementById('votes').innerHTML=counter +" votes";
+}
+function downvote() {
+    counter--;
+    document.getElementById('votes').innerHTML=counter +" votes";
+}
